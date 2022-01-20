79c20664a1dbd5feaf20633188807c0d364917b4
minsung8 <alstjddl8@naver.com>
2022-01-20 22:28:57 +0900

    DB 연결 테스트

--- mysql/handler.py
+++ mysql/handler.py
@@ -3,6 +3,7 @@ import pymysql
 
 def handle(req):
 
+    # mongoDB와 연결 해보기
     ret = []
     db = pymysql.connect(host='127.0.0.1', port=3306, user='local', db='storelink_vaccumator', password='local1234', charset='utf8')
     curs = db.cursor()
