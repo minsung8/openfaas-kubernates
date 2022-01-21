24141afbeddb007c9ab2ca532f0d4f58552b972d
minsung8 <alstjddl8@naver.com>
2022-01-22 03:53:08 +0900

    DB 테스트

--- mysql/handler.py
+++ mysql/handler.py
@@ -5,8 +5,6 @@ def handle(req):
 
     # mongoDB와 연결 해보기
     ret = []
-    db = pymysql.connect(host='127.0.0.1', port=3306, user='local', db='storelink_vaccumator', password='local1234', charset='utf8')
-    curs = db.cursor()
 
     return 'success'
     # sql = "select * from community_ppomppu_board;"
